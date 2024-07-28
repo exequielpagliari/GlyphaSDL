d8dea53dd42c6577b47a2f6a1a951bef3bc58169
Exequiel Pagliari <exepa@hotmail.com>
2024-07-28 09:29:47 -0300

    Primera Prueba Cmake

---
+++ includes/structs.h
@@ -0,0 +1,36 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <SDL.h>
+
+typedef struct {
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+} App;
+
+typedef struct {
+	int x;
+	int y;
+	SDL_Texture* texture;
+
+} Entity;
+
+
+typedef struct {
+	int x;
+	int y;
+	SDL_Texture* texture;
+	SDL_Rect* rect;
+
+} AnimateEntity;
+
+typedef struct {
+	Entity fighterHead, * fighterTail;
+	Entity bulletHead, * bulletTail;
+} Stage;
+
+
+typedef struct Textures {
+	SDL_Texture* texture;
+}   Textures;
+#endif // STRUCTS_H
---
+++ main.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+
+Textures flameSrcMap;
+Textures back;
+Textures obThunder;
+
+App app;
+int main(int argc, char* argv[])
+{
+
+	memset(&app, 0, sizeof(App));
+
+	//memset(&player, 0, sizeof(Entity));
+
+	initSDL();
+	initSounds();
+	InitVariables();
+	int i = 0;	
+
+
+	
+
+	
+
+	
+	LogNextTick(100);
+	
+	Spark = false;
+
+
+	//atexit(cleanup);
+
+	while (1)
+	{
+		printf("%d", Spark);
+		prepareScene();
+		
+		doInput();
+
+
+		
+		SDL_RenderCopy(app.renderer, back.texture, &backSrcRect,
+			&backSrcRect);
+
+
+		if (flameSrcRect.y > 32)
+			flameSrcRect.y = 0;
+
+		SDL_RenderCopy(app.renderer, flameSrcMap.texture, &flameSrcRect,
+			&flameDestRects[0]);
+		SDL_RenderCopy(app.renderer, flameSrcMap.texture, &flameSrcRect,
+			&flameDestRects[1]);
+		RenderTorchA();
+		RenderTorchB();
+
+		//SDL_RenderDrawLine(app.renderer, punto1.x, punto1.y, punto2.x, punto2.y);
+		
+		
+
+		
+		//WaitForNextTick();
+		
+		/*
+		SDL_RenderCopy(app.renderer, obeliskSrcMap, &obSrcRect,
+			&obeliskRects[0]);
+		
+		SDL_RenderCopy(app.renderer, obeliskSrcMap, &obSrcRect,
+			&obeliskRects[1]);
+		*/
+
+		
+
+
+
+		
+		WaitForNextTick();
+		LogNextTick(50);
+		
+
+
+		WaitForNextTick();
+		
+		LogNextTick(50);
+		//printf("%d\n", SDL_GetTicks64());
+
+
+
+		flameSrcRect.y += 16;
+		i++;
+		presentScene();
+
+		SDL_Delay(0);
+	}
+
+	return 0;
+}
+
+
+
